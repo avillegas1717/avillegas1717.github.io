eac111b3eb97ca42ae6a0291451fe04a1f3e731e
Alicia Villegas <134570292+avillegas1717@users.noreply.github.com>
2024-09-22 19:04:05 -0500

    created react app and site template

---
+++ avillegas1717-react/src/App.js
@@ -0,0 +1,121 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="full-page">
+          <div className="div">
+            <div className="frame">
+              <div className="text-wrapper">Alicia Villegas</div>
+              <p className="p">
+                Building enjoyable experiences for the web one line of code at a
+                time.
+              </p>
+            </div>
+            <div className="frame-2">
+              <div className="frame-3">
+                <div className="group">
+                  <img
+                    className="img"
+                    alt="Group"
+                    src="https://c.animaapp.com/cXb6EWys/img/group-1-2@2x.png"
+                  />
+                </div>
+                <div className="frame-4">
+                  <div className="text-wrapper-2">Skillset</div>
+                  <p className="text-wrapper-3">
+                    Alicia is a passionate developer committed to building web
+                    experiences that are intuitive, inclusive, and accessible to
+                    all.
+                  </p>
+                </div>
+              </div>
+              <div className="frame-3">
+                <div className="group-wrapper">
+                  <img
+                    className="img"
+                    alt="Group"
+                    src="https://c.animaapp.com/cXb6EWys/img/group-1-2@2x.png"
+                  />
+                </div>
+                <div className="frame-4">
+                  <div className="text-wrapper-2">Skillset</div>
+                  <p className="text-wrapper-3">lorem ipsum</p>
+                </div>
+              </div>
+              <div className="frame-3">
+                <div className="img-wrapper">
+                  <img
+                    className="img"
+                    alt="Group"
+                    src="https://c.animaapp.com/cXb6EWys/img/group-1-2@2x.png"
+                  />
+                </div>
+                <div className="frame-4">
+                  <div className="text-wrapper-2">Skillset</div>
+                  <p className="text-wrapper-3">lorem ipsum</p>
+                </div>
+              </div>
+            </div>
+            <div className="frame-5">
+              <img
+                className="group-2"
+                alt="Group"
+                src="https://c.animaapp.com/cXb6EWys/img/group-2@2x.png"
+              />
+              <p className="alicia-villegas">
+                {/* <span className="span">Alicia Villegas </span> */}
+                <span className="text-wrapper-4">Software Engineer </span>
+                <break></break>
+                <span className="text-wrapper-4"> | </span>
+                <span className="text-wrapper-5">
+                  Coding &amp; Cocktails Co-Presentation Director
+                </span>
+              </p>
+            </div>
+            <p className="text-wrapper-6">
+              Alicia is a passionate developer committed to building web
+              experiences that are intuitive, inclusive, and accessible to all.
+              With a strong advocacy for women in tech, she aims to create
+              digital solutions that empower diverse communities and foster a
+              more equitable tech industry. She believes that technology should
+              be both seamless and accessible, and she is driven by the
+              challenge of creating user-friendly designs that everyone can
+              enjoy.
+            </p>
+            <div className="frame-6">
+              <div className="div-wrapper">
+                <div className="text-wrapper-7">View Work</div>
+              </div>
+              <img
+                className="vector"
+                alt="Vector"
+                src="https://c.animaapp.com/cXb6EWys/img/vector.svg"
+              />
+            </div>
+            <div className="frame-7">
+              <div className="text-wrapper-8">Alicia Villegas</div>
+              <div className="text-wrapper-9">amv1717@gmail.com</div>
+            </div>
+            <div className="group-3">
+              <img
+                className="line"
+                alt="Line"
+                src="https://c.animaapp.com/cXb6EWys/img/line-2.svg"
+              />
+              <img
+                className="line-2"
+                alt="Line"
+                src="https://c.animaapp.com/cXb6EWys/img/line-2.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
